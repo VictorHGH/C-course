c7bc7d63a7c615d846164cc2d8e6c3f30cf2dad4
VictorHGH <64684570+VictorHGH@users.noreply.github.com>
2024-08-23 01:55:59 -0600

    [Updated] first class in OOP file

--- 014_POO/001_Clases_y_Objetos/main.cpp
+++ 014_POO/001_Clases_y_Objetos/main.cpp
@@ -5,13 +5,16 @@
 using namespace std;
 
 class Persona {
+
 public:
   string nombre;
   int edad;
+
   Persona(string nombre, int edad) {
     this->nombre = nombre;
     this->edad = edad;
   }
+
   void mostrarDatos() {
     cout << "Nombre: " << nombre << endl;
     cout << "Edad: " << edad << endl;
