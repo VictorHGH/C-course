613c6ba2a63db84839edb1532b781ae8a3428574
VictorHGH <64684570+VictorHGH@users.noreply.github.com>
2024-08-23 01:54:11 -0600

    [Updated] file on herencia class of OOP

--- 014_POO/006_Herencia/Triangulo.cpp
+++ 014_POO/006_Herencia/Triangulo.cpp
@@ -12,6 +12,7 @@ public:
     this->lado2 = lado2;
     this->lado3 = lado3;
   }
+
   float areaTriangulo() {
     float p = (lado1 + lado2 + lado3) / 2;
     float area = sqrt(p * (p - lado1) * (p - lado2) * (p - lado3));
