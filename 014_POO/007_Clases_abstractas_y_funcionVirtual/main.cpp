8610b672fc16efed21d3c7c92059aa3b5d3a1277
VictorHGH <64684570+VictorHGH@users.noreply.github.com>
2024-08-23 02:05:27 -0600

    [Updated] new animal herbivoro in OOP class

--- 014_POO/007_Clases_abstractas_y_funcionVirtual/main.cpp
+++ 014_POO/007_Clases_abstractas_y_funcionVirtual/main.cpp
@@ -1,14 +1,21 @@
 #include "AnimalCarnivoro.cpp"
+#include "AnimalHerbivoro.cpp"
 #include "Planta.cpp"
 using namespace std;
 
 int main() {
+
   Planta *planta1 = new Planta();
   planta1->alimentarse();
-
   cout << endl;
+
   AnimalCarnivoro *animalCarnivoro1 = new AnimalCarnivoro();
   animalCarnivoro1->alimentarse();
+  cout << endl;
+
+  AnimalHerbivoro *animalHerbivoro1 = new AnimalHerbivoro();
+  animalHerbivoro1->alimentarse();
+  cout << endl;
 
   return 0;
 }
